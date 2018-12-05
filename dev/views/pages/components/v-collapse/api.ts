fed87c45d0ead7c135bc9cd7837571ae6a8fe085
Alison <zhangyinag@126.com>
2018-12-05 18:45:40 +0800

    collapse

---
+++ dev/views/pages/components/v-collapse/api.ts
@@ -0,0 +1,55 @@
+const collapse = {
+  props: [
+    {
+      name: 'value',
+      default: '',
+      type: `string | string[]`,
+      desc: '当前激活面板名称, accordin下为单值, 可使用v-model'
+    },
+    {
+      name: 'accordion',
+      default: ``,
+      type: `boolean`,
+      desc: '手风琴效果， 一次只能打开一个面板'
+    },
+    {
+      name: 'bordered',
+      default: 'true',
+      type: `boolean`,
+      desc: '边框模式'
+    }
+  ]
+}
+
+const collapseItem = {
+  props: [
+    {
+      name: 'name',
+      default: '',
+      type: `string`,
+      desc: '面板名称， 唯一标识， 确保唯一性'
+    },
+    {
+      name: 'title',
+      default: ``,
+      type: `string`,
+      desc: '面板标题'
+    },
+    {
+      name: 'disabled',
+      default: '',
+      type: 'boolean',
+      desc: '禁用'
+    }
+  ]
+}
+
+export default [
+  {
+    name: 'Collapse API',
+    content: collapse
+  },
+  {
+    name: 'CollapseItem API',
+    content: collapseItem
+  }]
---
+++ src/components/collapse/VCollapse.vue
@@ -0,0 +1,53 @@
+<template>
+<div :class="[b(), borderedCls]"><slot></slot></div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop, Emit, Watch, Model, Provide } from 'vue-property-decorator'
+import { mixins } from 'vue-class-component'
+import Themeable from '@/mixins/Themeable'
+import Bemable from '@/mixins/Bemable'
+import Group from '@/mixins/Group'
+
+@Component({
+  components: {
+  },
+  name: 'v-collapse'
+  })
+export default class VCollapse extends mixins(Themeable, Bemable, Group) {
+  @Prop([String, Array]) @Model('input') value!: string | string[]
+
+  @Prop(Boolean) accordion!: boolean
+
+  @Prop({type: Boolean, default: true}) bordered!: boolean
+
+  @Emit() input (value: string | string[]) {}
+
+  get borderedCls () {
+    return this.bordered ? this.m('bordered') : ''
+  }
+
+  @Provide() toggle (name: string) {
+    if (this.accordion) {
+      this.input(name === this.value ? '' : name)
+    } else {
+      let has = this.value.includes(name)
+      if (has) {
+        this.input((this.value as string[]).filter(v => v !== name))
+      } else {
+        this.input([name, ...this.value])
+      }
+    }
+  }
+
+  @Provide() isActive (name: string) {
+    if (this.accordion) return name === this.value
+    return this.value.includes(name)
+  }
+
+  @Provide() getBordered () {
+    return this.bordered
+  }
+}
+</script>
+
+<style src="./style/v-collapse.scss" lang="scss"></style>
