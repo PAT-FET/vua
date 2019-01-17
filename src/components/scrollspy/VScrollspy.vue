ba20694651ccef502633047a65b3600fc1019c63
Alison <zhangyinag@126.com>
2019-01-17 16:23:41 +0800

    save temp

---
+++ src/components/scrollspy/VScrollspy.vue
@@ -0,0 +1,23 @@
+<template>
+<div :class="[b()]">
+  <slot></slot>
+</div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop, Emit, Watch, Model, Provide } from 'vue-property-decorator'
+import { mixins } from 'vue-class-component'
+import { Bemable, Themeable, Group } from '../../mixins'
+
+@Component({
+  components: {
+  },
+  name: 'v-scrollspy'
+  })
+export default class VScrollspy extends mixins(Themeable, Bemable, Group) {
+  @Prop(String) container!: string
+
+  @Prop({type: Number, default: 0}) offset!: number
+}
+</script>
+
+<style src="./style/v-scrollspy.scss" lang="scss"></style>
---
+++ src/components/scrollspy/VScrollspyItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="[b()]">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop, Emit, Watch, Model, Inject } from 'vue-property-decorator'
+import { mixins } from 'vue-class-component'
+import { Bemable, Themeable, Groupable } from '../../mixins'
+
+@Component({
+  components: {
+  },
+  name: 'v-scrollspy-item'
+  })
+export default class VScrollspyItem extends mixins(Themeable, Bemable, Groupable) {
+  @Prop(String) href!: string
+}
+</script>
+<style src="./style/v-scrollspy-item.scss" lang="scss"></style>
