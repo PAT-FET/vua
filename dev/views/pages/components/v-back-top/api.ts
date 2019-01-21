7b0c595e6aad5983f0782bdf5903a787a7c55a3c
wordlz <1036986847@qq.com>
2019-01-21 17:04:27 +0800

    误改恢复

--- dev/views/pages/components/v-back-top/api.ts
+++ dev/views/pages/components/v-back-top/api.ts
@@ -4,14 +4,14 @@ export default {
     {
       name: 'target',
       default: '',
-      type: ``,
-      desc: ''
+      type: `BackTopTargetFn`,
+      desc: '监听滚动元素，默认为 parentElement 或 body'
     },
     {
       name: 'offset',
       default: `200`,
-      type: ``,
-      desc: ''
+      type: `number`,
+      desc: '滚动高度达到此参数值才出现 BackTop'
     }
   ]
 }
